d5451ba86e09fda626770d6ea628b7728e2f46a4
ShantanuR1 <shantanu10491@gmail.com>
2026-02-21 00:19:48 +0530

    Initial Commit

---
+++ Task1.py
@@ -0,0 +1,12 @@
+num1 = int(input("enter 1st number:"))
+num2 = int(input("enter 2nd number:"))
+Addition = num1+num2
+subtraction = num1-num2
+multiplication = num1*num2
+Division = num1 / num2
+Modulus = num1 % num2
+print(f"Addition of {num1} and {num2} is {Addition}")
+print(f"subtraction of {num1} and {num2} is {subtraction}")
+print(f"multiplication of {num1} and {num2} is {multiplication}")
+print(f"Division of {num1} and {num2} is {Division}")
+print(f"Modulus of {num1} and {num2} is {Modulus}")
---
+++ Task2.py
@@ -0,0 +1,5 @@
+fn = input("enter your first name: ")
+ln = input("enter your last name: ")
+
+print(f"Hello, {fn} {ln}! Welcome to the Python Programming Language")
+
